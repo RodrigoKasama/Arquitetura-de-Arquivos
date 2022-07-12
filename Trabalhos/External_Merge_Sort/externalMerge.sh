287cef6fcf4f5acdf8f5f579ad398a421f1d9f9d
RodrigoKasama <rodrigo.parracho@gmail.com>
2022-07-11 22:15:41 -0300

    Update MergeSort script

--- Trabalhos/External_Merge_Sort/externalMerge.sh
+++ Trabalhos/External_Merge_Sort/externalMerge.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-FILENAME="cep.dat";
+if [ $# -ne "2" ]; then
+   echo "--Usage: $0 <n_particoes> <database_path>"
+   exit 1
+fi
+
+N_SEG=$(echo "2^$1" | bc);
+FILENAME=$2;
+
 CREATIONFILES="criando_filhos";
 JOINFILES="juntando_filhos";
-N_SEG=$1;
+
 Creation_bin_name="n_external";
 Joining_bin_name="inter";
-
 
 if [[ ! -d "$(pwd)/$CREATIONFILES" ]]; then
     mkdir "$(pwd)/$CREATIONFILES";
@@ -15,13 +21,6 @@ fi
 if [[ ! -d "$(pwd)/$JOINFILES" ]]; then
     mkdir "$(pwd)/$JOINFILES";
 fi
-
-# if [ *.dat ]; then
-#     echo "Existe arquivo .dat aqui";
-# else 
-#     echo "Nao ha arquivo .dat"
-# fi
-# exit 1;
 
 if [[ ! -e "$(pwd)/$FILENAME" ]]; then
     echo "Nao foi possivel encontrar a base de dados $Filename. Verifique se ela se encontra no diretorio '$(pwd)/$CREATIONFILES'";
@@ -47,7 +46,7 @@ if [[ ! -e "./$Creation_bin_name" ]]; then
     exit 1;
 fi
 
-./$Creation_bin_name $N_SEG && echo "Programa de particao ($Creation_bin_name) executado com sucesso" && rm ./$Creation_bin_name;
+./$Creation_bin_name $N_SEG $FILENAME && echo "Programa de particao ($Creation_bin_name) executado com sucesso" && rm ./$Creation_bin_name;
 
 mv cep_* "../$JOINFILES"
 
@@ -57,6 +56,7 @@ if [[ ! -e "./$Joining_bin_name" ]]; then
     echo "Nao foi possivel encontrar o binario $Joining_bin_name. Verifique se ela se encontra no diretorio '$(pwd)/$CREATIONFILES'";
     exit 1;
 fi
+
 ./$Joining_bin_name $N_SEG && echo "Programa de juncao ($Joining_bin_name) executado com sucesso" && rm ./$Joining_bin_name;
 
 echo "O arquivo remanescente equivale ao original.. :)"
